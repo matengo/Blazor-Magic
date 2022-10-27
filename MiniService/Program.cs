364a89c05cd48f70313969c3f2833adab4b58a34
matengo <mattias@adocka.com>
2022-10-27 11:20:52 +0200

    Some cleanup

--- MiniService/Program.cs
+++ MiniService/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MiniService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,34 +19,6 @@ await using var app = builder.Build();
 app.UseCors("default");
 app.UseGrpcWeb();
 
-app.MapGet("/sum/{x}/{y}", async (http) => {
-
-    if (!http.Request.RouteValues.TryGetValue("x", out var x) || !http.Request.RouteValues.TryGetValue("y", out var y))
-    {
-        http.Response.StatusCode = 400;
-        return;
-    }
-
-    var service = http.RequestServices.GetService<IMyFirstService>();
-    var result = await service.SumAsync(int.Parse(x.ToString()), int.Parse(y.ToString()));
-    await http.Response.WriteAsync(result.ToString());
-
-});
 app.MapMagicOnionService().EnableGrpcWeb();
 
 await app.RunAsync();
-
-public class MyFirstService : ServiceBase<IMyFirstService>, IMyFirstService
-{
-    private readonly ILogger<MyFirstService> _logger;
-    public MyFirstService(ILogger<MyFirstService> logger)
-    {
-        _logger = logger;
-    }
-    public async UnaryResult<int> SumAsync(int x, int y)
-    {
-        _logger.LogInformation("We are doing a mini grpc service");
-
-        return x + y;
-    }
-}
---
+++ MiniService/Services/MyFirstService.cs
@@ -0,0 +1,21 @@
+﻿using MagicOnion.Server;
+using MagicOnion;
+using Microsoft.Extensions.Logging;
+
+namespace MiniService.Services
+{
+    public class MyFirstService : ServiceBase<IMyFirstService>, IMyFirstService
+    {
+        private readonly ILogger<MyFirstService> _logger;
+        public MyFirstService(ILogger<MyFirstService> logger)
+        {
+            _logger = logger;
+        }
+        public async UnaryResult<int> SumAsync(int x, int y)
+        {
+            _logger.LogInformation("We are doing a mini grpc service");
+
+            return x + y;
+        }
+    }
+}
